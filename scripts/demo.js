23956be8450e7477169034a686ae1b6b23d4000e
lasha <dularidze.lasha@gmail.com>
2026-05-13 01:08:28 +0200

    [demo] testing

--- scripts/demo.js
+++ scripts/demo.js
@@ -8,14 +8,11 @@ launchBtn.addEventListener('click', async () => {
     loadingMsg.classList.remove('demo-hidden');
     demoContainer.classList.remove('demo-hidden');
 
-    const test = await fetch("/INF102_Team_1-website/demo/zaroc-demo.jar");
-    console.log("JAR status:", test.status, "Size:", test.headers.get("content-length"));
-
     await cheerpjInit({
         clipboardMode: "system",
         scalingFactor: window.devicePixelRatio,
         displayCanvas: document.getElementById('cheerpjDisplay')
     });
 
-    await cheerpjRunJar("/INF102_Team_1-website/demo/zaroc-demo.jar");
+    await cheerpjRunJar("/demo/zaroc-demo.jar");
 });
