f39c27ed34965a95a5ef8c41b316e38a31cfebdb
lasha <dularidze.lasha@gmail.com>
2026-05-20 22:05:56 +0200

    [demo] JPro - fixed src for HTTPS

--- scripts/demo.js
+++ scripts/demo.js
@@ -2,5 +2,5 @@ document.getElementById('launch-btn').addEventListener('click', function() {
     document.getElementById('demo-container').classList.remove('demo-hidden');
     document.getElementById('loading-msg').classList.add('demo-hidden');
 
-    document.getElementById('jpro-frame').src = "http://10.134.177.16/:8080";
+    document.getElementById('jpro-frame').src = "/demo/";
 });
